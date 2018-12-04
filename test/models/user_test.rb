0213e13e6db02e40c793ccbc4f1e6a8ec75906e9
KW <werniokrzysztof@gmail.com>
2018-12-04 15:09:36 +0100

    validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,30 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "example user", email: "example@gmail.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should be present" do
+    @user.name = "Korzonek"
+    @user.email = "korzonek@drzewo.gmailus.com"
+    assert @user.valid?
+  end
+
+  test "not too long" do
+    @user.name = "a"*53
+    assert_not @user.valid?
+  end
+
+  test "email unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email.upcase!
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end

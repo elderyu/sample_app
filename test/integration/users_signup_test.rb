5410a0b5897eb5e49ec4ed4e8f231cbf20adf793
KW <werniokrzysztof@gmail.com>
2018-12-05 13:45:44 +0100

    working signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "li", "Password can't be blank", count: 2
     #assert_select 'div.<CSS class for field with error>'
   end
+
+  test "vaild signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params:{user: {name: "example user", email: "example@user.com.pl", password: "1234567890", password_confirmation: "1234567890"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "div[class=?]", "alert alert-success"
+    assert_not flash[:success].blank?
+  end
+
 end

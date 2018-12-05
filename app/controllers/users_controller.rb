6fbe7faef7d225f9a642468d8e49cd1c386383ec
KW <werniokrzysztof@gmail.com>
2018-12-05 11:17:32 +0100

    sign up page working, rendering new if invalid, not adding users yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 #require 'byebug'
 class UsersController < ApplicationController
+
   def new
+    @user = User.new(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render 'new'
+    end
+
+  end
+
   def show
     @user = User.find(params[:id])
     #byebug
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+
+  def gravatar_for(user, size: 150)
+    gravatar_id = Digest::MD5::hexdigest user.email  # advisable to use with downcase, but downcasing before save already present in user.rb model
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
+
 end
+
+#alternative
+
+#   # Returns the Gravatar for the given user.
+#   def gravatar_for(user, options = { size: 80 })
+#     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+#     size = options[:size]
+#     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+#     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+#   end
+# end

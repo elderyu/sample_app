76cc64d432d3464525a1b46b202a426703bb6b71
KW <werniokrzysztof@gmail.com>
2018-12-06 15:11:19 +0100

    application.js order changed and bootstrap-sprockets removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
-//= require bootstrap/dropdown
 //= require rails-ujs
 //= require activestorage
 // require turbolinks
